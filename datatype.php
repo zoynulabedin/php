3bc3a99ba8a14704abe9434899bed879eeed48ce
coderworker <trustcodernet@gmail.com>
2018-09-25 12:55:07 +0600

    updated

--- datatype.php
+++ datatype.php
@@ -24,3 +24,28 @@
 // $age      = 28;
 
 // printf('my nae is %s %s . my age is %s',$firstName,$lastName,$age);
+
+
+/**
+ * 
+ * Number + -
+ * */
+
+//  $number  = 12;
+//   $number += 52;
+
+//  echo $number;
+
+/*
+* operator
++ - * / $ = arithmatic operator
++= -= 
+*/
+
+// $x =$y=5;
+
+// echo $x,"\n",$y;
+
+// $name = $otherName = "fullname";
+
+// echo $name,"\n",$otherName;
